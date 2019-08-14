e11be0d6dd485ebc2856392b454e9aa1ffa2439f
Anoop M D <anoop.md1421@gmail.com>
2019-08-14 16:20:26 +0530

    oj Model

--- src/js/viewModels/expenses.js
+++ src/js/viewModels/expenses.js
@@ -1,12 +1,22 @@
 define(['ojs/ojmodel'], function(){
   console.log(oj.Model);
 
-  var user = new oj.Model({
-    name: 'Anoop',
-    age: 28,
+  var Expense = oj.Model.extend({
+    urlRoot: 'http://localhost:5000/api/expense',
   });
 
-  console.log(user.toJSON());
-  console.log(user.has('age1'));
+  var ex = new Expense({
+    id: '6313a073d5018e96'
+  });
+
+  ex
+    .fetch()
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
+
   return {};
 });
