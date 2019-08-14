89fc9c511fc9f63a4920a252de45101f3860235d
Anoop M D <anoop.md1421@gmail.com>
2019-08-14 15:32:45 +0530

    Stuff

--- src/js/viewModels/expenses.js
+++ src/js/viewModels/expenses.js
@@ -1,3 +1,12 @@
-define([], function(){
+define(['ojs/ojmodel'], function(){
+  console.log(oj.Model);
+
+  var user = new oj.Model({
+    name: 'Anoop',
+    age: 28,
+  });
+
+  console.log(user.toJSON());
+  console.log(user.has('age1'));
   return {};
 });
