ede049af68a6ac2c06b27824e73d515d7a207dc9
Anoop M D <anoop.md1421@gmail.com>
2019-08-16 07:01:56 +0530

    Edit functionality

--- src/js/viewModels/expenses.js
+++ src/js/viewModels/expenses.js
@@ -2,14 +2,15 @@ define(['models/expense', 'models/expenses', 'knockout', 'ojs/ojinputtext',
   'ojs/ojformlayout', 'ojs/ojlabel',
   'ojs/ojbutton', 'ojs/ojcollectiondataprovider',
   'ojs/ojpagingdataproviderview', 'ojs/ojpagingcontrol',
-  'ojs/ojtable', 'ojs/ojinputnumber'], function(Expense, Expenses, ko){
+  'ojs/ojtable', 'ojs/ojinputnumber', 'ojs/ojdialog'], function(Expense, Expenses, ko){
   
   function ViewModel() {
     var self = this;
 
     this.columnOptions = [
       {"headerText": "Description", "field": "description"},
-      {"headerText": "Amount", "field": "amount"}];
+      {"headerText": "Amount", "field": "amount"},
+      {"headerText": "Actions", "template": "actionTemplate"}];
 
     this.expenses = new Expenses();
     this.expenses
@@ -25,7 +26,16 @@ define(['models/expense', 'models/expenses', 'knockout', 'ojs/ojinputtext',
     this.pagingDataProvider = ko.observable();
     this.pagingDataProvider(new oj.PagingDataProviderView(this.dataProvider));
 
+    this.openEditModal = function(data, componentElement) {
+      document.getElementById("editExpenseDialog").open();
+    };
+
     this.form = {
+      description: ko.observable(),
+      amount: ko.observable()
+    };
+
+    this.editForm = {
       description: ko.observable(),
       amount: ko.observable()
     };
