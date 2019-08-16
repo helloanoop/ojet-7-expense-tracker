a14e46ed30acc67f0a94aae42b6cd502815191e3
Anoop M D <anoop.md1421@gmail.com>
2019-08-16 05:59:03 +0530

    Implemented Pagination + 'amount' field + Reset form

--- src/js/models/expenses.js
+++ src/js/models/expenses.js
@@ -1,7 +1,30 @@
 define(['./expense', 'ojs/ojmodel'], function(Expense){
   var Expenses = oj.Collection.extend({
     url: 'http://localhost:5000/api/expense',
-    model: Expense
+    model: Expense,
+    customURL: function(operation, collection, options) {
+      if(!isNaN(options.fetchSize) && !isNaN(options.startIndex)) {
+        return `http://localhost:5000/api/expense?$limit=${options.fetchSize}&$skip=${options.startIndex}`;
+      }
+
+      return 'http://localhost:5000/api/expense';
+    },
+    paginatedRequestUrl: function(options) {
+      return `${this.url}?$limit=${options.fetchSize}&$skip=${options.startIndex}`;
+    },
+    customPagingOptions: function(response) {
+      if(!response || !response.data) {
+        return response;
+      }
+
+      return {
+        totalResults: response.total,
+        limit: response.limit,
+        count: response.data.length,
+        offset: response.skip,
+        hasMore: (response.skip + response.data.length) < response.total
+      };
+    }
   });
 
   return Expenses;
--- src/js/viewModels/expenses.js
+++ src/js/viewModels/expenses.js
@@ -1,33 +1,52 @@
 define(['models/expense', 'models/expenses', 'knockout', 'ojs/ojinputtext',
   'ojs/ojformlayout', 'ojs/ojlabel',
   'ojs/ojbutton', 'ojs/ojcollectiondataprovider',
-  'ojs/ojtable'], function(Expense, Expenses, ko){
+  'ojs/ojpagingdataproviderview', 'ojs/ojpagingcontrol',
+  'ojs/ojtable', 'ojs/ojinputnumber'], function(Expense, Expenses, ko){
   
   function ViewModel() {
     var self = this;
 
     this.columnOptions = [
       {"headerText": "Description", "field": "description"},
-      {"headerText": "Date", "field": "date"}];
+      {"headerText": "Amount", "field": "amount"}];
 
     this.expenses = new Expenses();
-    this.expenses.fetch()
-    .then(function() {
-      console.log(self.expenses.toJSON());
-    });
+    this.expenses
+      .fetch({
+        startIndex: 0,
+        fetchSize: 5
+      })
+      .then(function() {
+        console.log(self.expenses.toJSON());
+      });
 
-    this.dataprovider = new oj.CollectionDataProvider(this.expenses);
+    this.dataProvider = new oj.CollectionDataProvider(this.expenses);
+    this.pagingDataProvider = ko.observable();
+    this.pagingDataProvider(new oj.PagingDataProviderView(this.dataProvider));
 
     this.form = {
-      description: ko.observable()
+      description: ko.observable(),
+      amount: ko.observable()
     };
 
     this.onCreate = function() {
       var ex = new Expense();
 
       ex.set('description', self.form.description());
-      ex.save();
+      ex.set('amount', self.form.amount());
+      ex
+        .save()
+        .then(function() {
+          self.expenses.add(ex);
+          self.resetForm();
+        });
     };
+
+    this.resetForm = function() {
+      self.form.description('');
+      self.form.amount(null);
+    }
   }
 
   return ViewModel;
